033fd1b341b7be96fde92a291de4991c5b1c9f3d
Saza-ku <the.latticeheart@gmail.com>
2025-05-19 20:52:54 +0900

    wip

--- rstest/tests/resources/rstest/ignore_not_fixture_arg.rs
+++ rstest/tests/resources/rstest/ignore_not_fixture_arg.rs
@@ -14,3 +14,15 @@ fn my_fixture() -> FixtureStruct {
 async fn test_db(my_fixture: FixtureStruct, #[ignore] pool: SqlitePool) {
     assert!(true);
 }
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[rstest]
+#[case(1, 2)]
+#[case(2, 3)]
+#[sqlx::test]
+fn test_sum(#[case] a: i32, #[case] b: i32, #[ignore] pool: SqlitePool) {
+    assert_eq!(sum(a, b), a + b);
+}
