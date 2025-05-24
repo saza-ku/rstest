ed8e1bcc0842027143d4cbca8da1b277928f22ba
saza-ku <the.latticeheart@gmail.com>
2025-05-24 14:48:16 +0900

    use only actix_web::test and sqlx::test

--- rstest/tests/resources/rstest/ignore_attributes_args_if_any_with_actix_test.rs
+++ rstest/tests/resources/rstest/ignore_attributes_args_if_any_with_actix_test.rs
@@ -1,16 +1,14 @@
-use rstest::*;
-
 use sqlx::SqlitePool;
 
 fn sum(a: i32, b: i32, c: i32) -> i32 {
     a + b + c
 }
 
-#[rstest]
 #[actix_web::test]
 #[sqlx::test]
-async fn test_sum(#[values(1, 2)] c: i32, #[ignore] pool: SqlitePool) {
+async fn test_sum(pool: SqlitePool) {
     let a = 1;
     let b = 2;
+    let c = 3;
     assert_eq!(sum(a, b, c), a + b + c);
 }
